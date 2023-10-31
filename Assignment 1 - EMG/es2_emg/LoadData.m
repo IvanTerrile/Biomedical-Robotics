0ed65315cfc3253386244a965bc9786acba1c3f5
Davide Pisano <pisy1997@gmail.com>
2023-10-31 16:46:14 +0100

    Es 2_3 finito funziona !!!

--- Assignment 1 - EMG/es2_emg/LoadData.m
+++ Assignment 1 - EMG/es2_emg/LoadData.m
@@ -9,3 +9,13 @@ subplot(4,1,3);
 plot(ES2_emg.time,ES2_emg.signals(:,3));
 subplot(4,1,4);
 plot(ES2_emg.time,ES2_emg.signals(:,4));
+
+% Angolo di rotazione in gradi
+angolo = 45;
+
+% Converte l'angolo in radianti
+angolo_rad = deg2rad(angolo);
+
+% Matrice di rotazione 2D
+R = [cos(angolo_rad), -sin(angolo_rad) 0; sin(angolo_rad), cos(angolo_rad) 0; 0 0 1];
+
