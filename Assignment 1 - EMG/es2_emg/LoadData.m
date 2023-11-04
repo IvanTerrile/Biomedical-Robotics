efc0184d748f85444419919c9538c37e7e5c41f0
Ivanterry00 <97966280+Ivanterry00@users.noreply.github.com>
2023-11-04 16:19:29 +0100

    Riordinato il file LoadData e provato a capire come fare il 2_4, ma non ho cavato un ragno dal buco

--- Assignment 1 - EMG/es2_emg/LoadData.m
+++ Assignment 1 - EMG/es2_emg/LoadData.m
@@ -1,26 +1,38 @@
+% Load the data from 'ES2_emg.mat'
 load('ES2_emg.mat');
-%plot di ogni segnale isolato sull'asse dei tempi
+
+% Plot each isolated signal on the time axis
 figure(1);
+
+% Subplot 1 for the first signal
 subplot(4,1,1);
-plot(ES2_emg.time,ES2_emg.signals(:,1));
+plot(ES2_emg.time, ES2_emg.signals(:,1));
+
+% Subplot 2 for the second signal
 subplot(4,1,2);
-plot(ES2_emg.time,ES2_emg.signals(:,2));
+plot(ES2_emg.time, ES2_emg.signals(:,2));
+
+% Subplot 3 for the third signal
 subplot(4,1,3);
-plot(ES2_emg.time,ES2_emg.signals(:,3));
+plot(ES2_emg.time, ES2_emg.signals(:,3));
+
+% Subplot 4 for the fourth signal
 subplot(4,1,4);
-plot(ES2_emg.time,ES2_emg.signals(:,4));
+plot(ES2_emg.time, ES2_emg.signals(:,4));
 
-% Angolo di rotazione in gradi
-angolo = 45;
+% Rotation angle in degrees
+angle = 45;
 
-% Converte l'angolo in radianti
-angolo_rad = deg2rad(angolo);
+% Convert the angle to radians
+angle_rad = deg2rad(angle);
 
-% Matrice di rotazione 2D
-R = [cos(angolo_rad), -sin(angolo_rad) 0; sin(angolo_rad), cos(angolo_rad) 0; 0 0 1];
+% 2D Rotation matrix
+R = [cos(angle_rad), -sin(angle_rad),  0; 
+     sin(angle_rad),  cos(angle_rad),  0; 
+                  0,               0,  1];
 
-vel_br=out.Bicipe_right_vel;
-vel_bl=out.Bicipe_left_vel;
-vel_tr=out.Trapezius_Right_vel;
-vel_tl=out.Trapezius_Left_vel;
-
+% Velocity data for different muscles
+vel_br = out.Bicep_right_vel;
+vel_bl = out.Bicep_left_vel;
+vel_tr = out.Trapezius_Right_vel;
+vel_tl = out.Trapezius_Left_vel;
